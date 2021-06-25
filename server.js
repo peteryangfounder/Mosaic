4f6692132715b3fc8058cb4684312360a242c74c
Peter Yang <peter@reviewerly.io>
2021-06-24 21:21:36 -0400

    Connect to Mongo Atlas

--- server.js
+++ server.js
@@ -14,12 +14,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.use(methodOverride('_method'));
 
-// mongoose.connect(process.env.DATABASE_URL, {
-// 	useNewUrlParser: true,
-// 	useUnifiedTopology: true
-// });
-
-mongoose.connect('mongodb://localhost:27017/mosaicDB', {
+mongoose.connect(process.env.DATABASE_URL, {
 	useNewUrlParser: true,
 	useUnifiedTopology: true
 });
